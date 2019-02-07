5c8f7ad0ec567569307d7cae0cef3adeb150fdd9
lqhunter <2458717795@qq.con>
2019-02-07 21:39:49 +0800

    增加侧边栏

--- app/src/main/java/com/lq/myapp/MainActivity.java
+++ app/src/main/java/com/lq/myapp/MainActivity.java
@@ -3,11 +3,17 @@ package com.lq.myapp;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.hjm.bottomtabbar.BottomTabBar;
@@ -15,7 +21,7 @@ import com.lq.myapp.fragments.PicFragment;
 import com.lq.myapp.fragments.RadioFragment;
 import com.lq.myapp.fragments.VideoFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
     private Toolbar mToolbar;
@@ -34,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
         //initToolBar
         mToolbar = findViewById(R.id.test_tool_bar);
         setSupportActionBar(mToolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         BottomTabBar bottomTabBar = findViewById(R.id.bottom_tab_bar);
@@ -66,6 +80,16 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setCurrentTab(0);   //设置当前选中的Tab，从0开始
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -131,4 +155,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Log.d(TAG, "nav_camera...");
+
+            // Handle the camera action
+        } else if (id == R.id.nav_history) {
+            Log.d(TAG, "nav_gallery...");
+
+        } else if (id == R.id.nav_download) {
+            Log.d(TAG, "nav_slideshow...");
+
+        } else if (id == R.id.nav_favorite) {
+            Log.d(TAG, "nav_manage...");
+
+        } else if (id == R.id.nav_share) {
+            Log.d(TAG, "nav_share...");
+
+        } else if (id == R.id.nav_send) {
+            Log.d(TAG, "nav_send...");
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+        return true;
+    }
 }
